cebf2f503ff63c325d7e33cc677fb30f9ecc67c5
kwanok <cloq@kakao.com>
2022-09-29 17:42:44 +0900

    counting정렬 벤치마킹

--- go/sort-study/counting.go
+++ go/sort-study/counting.go
@@ -1,17 +1,36 @@
 package sort_study
 
-func CountingSort(slice []int, max int) []int {
-	count := make([]int, max+1)
-	for _, v := range slice {
+func CountingSortA(arr []int, k int) []int {
+	count := make([]int, k+1)
+	for _, v := range arr { // n 회
 		count[v]++
 	}
-	for i := 1; i <= max; i++ {
+
+	sorted := make([]int, 0, len(arr))
+	for i := 0; i < k+1; i++ {
+		for j := 0; j < count[i]; j++ { // n 회
+			sorted = append(sorted, i)
+		}
+	}
+
+	return sorted
+}
+
+func CountingSortB(arr []int, k int) []int {
+	count := make([]int, k+1)
+	for _, v := range arr { // n 회
+		count[v]++
+	}
+
+	for i := 1; i <= k; i++ { // k 회
 		count[i] += count[i-1]
 	}
-	result := make([]int, len(slice))
-	for _, v := range slice {
-		result[count[v]-1] = v
+
+	sorted := make([]int, len(arr))
+	for _, v := range arr { // n 회
+		sorted[count[v]-1] = v
 		count[v]--
 	}
-	return result
+
+	return sorted
 }
